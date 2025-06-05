a56217062609d4d085aafada1f958ea52fa94994
Salma HARBI <sharbi@sofrecom.fr>
2025-06-05 23:07:55 +0100

    new commit2

---
+++ msnotif/src/main/java/com/entreprise/msnotif/MsnotifApplication.java
@@ -0,0 +1,21 @@
+package com.entreprise.msnotif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@EnableConfigurationProperties
+@RefreshScope
+public class MsnotifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsnotifApplication.class, args);
+    }
+
+}
